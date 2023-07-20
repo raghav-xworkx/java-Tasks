6a13acb65e73d4e8a19ba0015239bf740be99979
raghav-xworkx <139941151+raghav-xworkx@users.noreply.github.com>
2023-07-19 17:27:45 -0800

    java Initial Tasks

---
+++ IfCondition.java
@@ -0,0 +1,11 @@
+public class IfCondition {
+	public static void main(String[] bob)
+	{
+		int x=100;
+		if (800>x)
+		{
+			System.out.println("800 is greater than 100");
+		}
+			
+	}
+}
---
+++ IfElse.java
@@ -0,0 +1,18 @@
+public class IfElse {
+	public static void main(String[] arg)
+	{
+		int number =90;
+		if ( number<100)
+		{
+			System.out.println("This number is correct");
+		}
+		
+		else
+		{
+			System.out.println("This number is not correcte");
+			
+		}
+		
+		
+	}
+}
